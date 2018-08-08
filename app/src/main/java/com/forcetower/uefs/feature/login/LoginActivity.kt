a00a97529d0555fb9f14da0ede1120e3b174c680
ForceTower <joaopaulo761@gmail.com>
2018-08-08 07:09:08 -0300

    Some minor changes

--- app/src/main/java/com/forcetower/uefs/feature/login/LoginActivity.kt
+++ app/src/main/java/com/forcetower/uefs/feature/login/LoginActivity.kt
@@ -2,16 +2,27 @@ package com.forcetower.uefs.feature.login
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.forcetower.uefs.R
 import com.forcetower.uefs.feature.shared.UActivity
+import com.forcetower.uefs.databinding.ActivityLoginBinding
+import com.forcetower.uefs.feature.shared.config
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : UActivity() {
+    private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
     }
 
     override fun navigateUpTo(upIntent: Intent?): Boolean = findNavController(R.id.login_nav_host).navigateUp()
+
+    override fun showSnack(string: String) {
+        val snack = Snackbar.make(binding.root, string, Snackbar.LENGTH_SHORT)
+        snack.config()
+        snack.show()
+    }
 }
