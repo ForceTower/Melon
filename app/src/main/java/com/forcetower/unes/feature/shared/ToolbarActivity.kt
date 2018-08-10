747663c9f0894691ea8ab8b055649d2e3e9ee227
ForceTower <joaopaulo761@gmail.com>
2018-08-10 09:56:58 -0300

    Tab Indicator Fight

--- app/src/main/java/com/forcetower/unes/feature/shared/ToolbarActivity.kt
+++ app/src/main/java/com/forcetower/unes/feature/shared/ToolbarActivity.kt
@@ -1,7 +1,11 @@
 package com.forcetower.unes.feature.shared
 
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.tabs.TabLayout
 
 interface ToolbarActivity {
-    fun getToolbar() : Toolbar
+    fun getToolbar(): Toolbar
+    fun getTabLayout(): TabLayout
+    fun getAppBar(): AppBarLayout
 }
