4d7f67eb8973150a54d4163015d42d63c2a8d3fa
ForceTower <joaopaulo761@gmail.com>
2018-08-13 12:58:07 -0300

    Messages Paged List

--- app/src/main/java/com/forcetower/unes/core/storage/database/dao/MessageDao.kt
+++ app/src/main/java/com/forcetower/unes/core/storage/database/dao/MessageDao.kt
@@ -1,10 +1,9 @@
 package com.forcetower.unes.core.storage.database.dao
 
-import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.forcetower.unes.core.model.Message
 
@@ -13,6 +12,6 @@ interface MessageDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertIgnoring(messages: List<Message>)
 
-    @Query("SELECT * FROM Message")
-    fun getAllMessages(): LiveData<List<Message>>
+    @Query("SELECT * FROM Message ORDER BY timestamp DESC")
+    fun getAllMessages(): DataSource.Factory<Int, Message>
 }
