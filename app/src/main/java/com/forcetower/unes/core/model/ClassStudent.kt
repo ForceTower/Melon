d1adaadea4ed720896f605c47745f162a988210f
ForceTower <joaopaulo761@gmail.com>
2018-08-13 19:18:55 -0300

    Grades !

--- app/src/main/java/com/forcetower/unes/core/model/ClassStudent.kt
+++ app/src/main/java/com/forcetower/unes/core/model/ClassStudent.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Student(
+data class ClassStudent(
     @PrimaryKey(autoGenerate = true)
     val uid: Long = 0,
     val profileId: Long,
