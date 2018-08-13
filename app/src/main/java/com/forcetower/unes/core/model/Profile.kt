4d4895171de2c13a4d1e261b782c9141963a7279
ForceTower <joaopaulo761@gmail.com>
2018-08-13 09:33:09 -0300

    Started Saving stuff to Database

--- app/src/main/java/com/forcetower/unes/core/model/Profile.kt
+++ app/src/main/java/com/forcetower/unes/core/model/Profile.kt
@@ -1,10 +1,13 @@
 package com.forcetower.unes.core.model
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
+import androidx.room.ForeignKey.CASCADE
 import java.util.*
 
-@Entity
+@Entity(indices = [
+    Index(value = ["sagres_id"], unique = true),
+    Index(value = ["uuid"], unique = true)
+])
 data class Profile(
     @PrimaryKey(autoGenerate = true)
     val uid: Long = 0,
@@ -13,6 +16,8 @@ data class Profile(
     val score: Double = -1.0,
     val course: Long? = null,
     val imageUrl: String? = null,
+    @ColumnInfo(name = "sagres_id")
     val sagresId: Long,
-    val uuid: String = UUID.randomUUID().toString()
+    val uuid: String = UUID.randomUUID().toString(),
+    val me: Boolean = false
 )
