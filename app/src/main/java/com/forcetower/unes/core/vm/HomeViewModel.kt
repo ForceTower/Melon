b9901c2146a054523a7f42f4654e4c2fe96ca382
ForceTower <joaopaulo761@gmail.com>
2018-08-13 12:58:07 -0300

    Messages Paged List

--- app/src/main/java/com/forcetower/unes/core/vm/HomeViewModel.kt
+++ app/src/main/java/com/forcetower/unes/core/vm/HomeViewModel.kt
@@ -1,10 +1,21 @@
 package com.forcetower.unes.core.vm
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import com.forcetower.unes.core.model.Access
+import com.forcetower.unes.core.model.Message
+import com.forcetower.unes.core.model.Profile
+import com.forcetower.unes.core.storage.repository.SagresDataRepository
 import com.forcetower.unes.core.storage.repository.UserRepository
 import javax.inject.Inject
 
-class HomeViewModel @Inject constructor(private val repository: UserRepository): ViewModel() {
-    fun getAccess() = repository.getAccess()
-    fun getProfile() = repository.getProfileMe()
+class HomeViewModel
+@Inject constructor(
+        private val userRepository: UserRepository,
+        private val dataRepository: SagresDataRepository
+): ViewModel() {
+    val access: LiveData<Access?> by lazy { userRepository.getAccess() }
+    val profile: LiveData<Profile> by lazy { userRepository.getProfileMe() }
+    val messages: LiveData<PagedList<Message>> by lazy { dataRepository.getMessages() }
 }
