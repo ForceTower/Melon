0a54fa0e96f154750498bfe2536707401fde321b
ForceTower <joaopaulo761@gmail.com>
2018-08-13 17:29:20 -0300

    Creating the Disciplines Database

---
+++ app/src/main/java/com/forcetower/unes/core/model/Student.kt
@@ -0,0 +1,13 @@
+package com.forcetower.unes.core.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Student(
+    @PrimaryKey(autoGenerate = true)
+    val uid: Long = 0,
+    val profileId: Long,
+    val classId: Long,
+    val finalGrade: Double?
+)
