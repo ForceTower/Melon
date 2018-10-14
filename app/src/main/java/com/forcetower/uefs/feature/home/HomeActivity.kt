1d2e11419662c7209d6f3c6ece5cf3a05a0eea4a
ForceTower <joaopaulo761@gmail.com>
2018-10-14 18:13:40 -0300

    Fixed Android 6.0 Bug

--- app/src/main/java/com/forcetower/uefs/feature/home/HomeActivity.kt
+++ app/src/main/java/com/forcetower/uefs/feature/home/HomeActivity.kt
@@ -34,6 +34,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.crashlytics.android.Crashlytics
 import com.forcetower.uefs.R
 import com.forcetower.uefs.core.model.unes.Access
 import com.forcetower.uefs.core.vm.EventObserver
@@ -96,6 +97,9 @@ class HomeActivity : UActivity(), HasSupportFragmentInjector {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
             startActivity(intent)
+        } else {
+            Crashlytics.setUserIdentifier(access.username)
+            Crashlytics.setUserName(firebaseAuth.currentUser?.email)
         }
     }
 
