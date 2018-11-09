c1bb78791d4ff74948bf5ccb47fc77540831f3d6
João Paulo Sena <joaopaulo761@gmail.com>
2018-11-09 20:52:39 -0300

    Spacings...

--- app/src/main/java/com/forcetower/uefs/architecture/service/bigtray/BigTrayService.kt
+++ app/src/main/java/com/forcetower/uefs/architecture/service/bigtray/BigTrayService.kt
@@ -5,7 +5,6 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.Observer
 import com.forcetower.uefs.core.model.bigtray.BigTrayData
