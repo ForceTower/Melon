584c7237c6bbce59ae08c5e18849cd29b2996f7f
João Sena <joaopaulo761@gmail.com>
2019-01-26 15:57:17 -0300

    :rocket: Shows the 3 preconditions

--- app/src/main/java/com/forcetower/uefs/easter/darktheme/DarkThemeUnlockFragment.kt
+++ app/src/main/java/com/forcetower/uefs/easter/darktheme/DarkThemeUnlockFragment.kt
@@ -31,11 +31,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.forcetower.uefs.core.injection.Injectable
+import com.forcetower.uefs.core.vm.UViewModelFactory
+import com.forcetower.uefs.databinding.FragmentDarkThemeUnlockBinding
 import com.forcetower.uefs.feature.shared.UFragment
+import com.forcetower.uefs.feature.shared.extensions.provideActivityViewModel
+import javax.inject.Inject
 
 class DarkThemeUnlockFragment : UFragment(), Injectable {
+    @Inject
+    lateinit var factory: UViewModelFactory
+    lateinit var viewModel: DarkThemeViewModel
+    lateinit var binding: FragmentDarkThemeUnlockBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return null
+        viewModel = provideActivityViewModel(factory)
+        return FragmentDarkThemeUnlockBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val preconditionsAdapter = PreconditionsAdapter()
+        binding.preconditionsRecycler.run {
+            adapter = preconditionsAdapter
+        }
+        viewModel.preconditions.observe(this, Observer { preconditionsAdapter.submitList(it) })
     }
 }
