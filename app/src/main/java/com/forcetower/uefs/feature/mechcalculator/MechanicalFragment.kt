7b6e4b70b9d7d855df61b920c64f54648a2bfc9f
João Sena <joaopaulo761@gmail.com>
2019-01-30 21:48:09 -0300

    :rocket: Mechanical Overdrive

---
+++ app/src/main/java/com/forcetower/uefs/feature/mechcalculator/MechanicalFragment.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2019.
+ * João Paulo Sena <joaopaulo761@gmail.com>
+ *
+ * This file is part of the UNES Open Source Project.
+ *
+ * UNES is licensed under the MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.forcetower.uefs.feature.mechcalculator
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.forcetower.uefs.R
+import com.forcetower.uefs.core.injection.Injectable
+import com.forcetower.uefs.core.vm.UViewModelFactory
+import com.forcetower.uefs.databinding.FragmentMechCalculatorBinding
+import com.forcetower.uefs.feature.shared.UFragment
+import com.forcetower.uefs.feature.shared.extensions.provideActivityViewModel
+import javax.inject.Inject
+
+class MechanicalFragment : UFragment(), Injectable {
+    @Inject
+    lateinit var factory: UViewModelFactory
+    lateinit var binding: FragmentMechCalculatorBinding
+    lateinit var viewModel: MechanicalViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewModel = provideActivityViewModel(factory)
+        return FragmentMechCalculatorBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.incToolbar.textToolbarTitle.text = getString(R.string.label_mech_calculator)
+    }
+}
