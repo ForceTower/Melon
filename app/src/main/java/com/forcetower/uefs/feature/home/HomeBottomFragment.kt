c9ed5f4c6cde640993a7a75db8d50aebbcd168d7
João Sena <joaopaulo761@gmail.com>
2019-02-01 21:55:53 -0300

    :rocket: General fixes

--- app/src/main/java/com/forcetower/uefs/feature/home/HomeBottomFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/home/HomeBottomFragment.kt
@@ -125,6 +125,9 @@ class HomeBottomFragment : UFragment(), Injectable {
 
         val hourglass = remoteConfig.getBoolean("feature_flag_hourglass")
         toggleItem(R.id.hourglass, hourglass)
+
+        val dark = preferences.getBoolean("stg_night_mode_menu", true)
+        toggleItem(R.id.dark_theme_event, dark)
     }
 
     private fun toggleNightModeSwitcher(enabled: Boolean?) {
