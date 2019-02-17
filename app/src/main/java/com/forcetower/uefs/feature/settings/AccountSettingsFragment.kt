305d04e0d390fc4047bbc60e1d5c2a25d0f44e09
João Sena <joaopaulo761@gmail.com>
2019-02-17 11:59:16 -0300

    :rocket: Mark calculated score downloads all grades
    Closes #39

--- app/src/main/java/com/forcetower/uefs/feature/settings/AccountSettingsFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/settings/AccountSettingsFragment.kt
@@ -27,12 +27,52 @@
 
 package com.forcetower.uefs.feature.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.preference.PreferenceFragmentCompat
 import com.forcetower.uefs.R
+import com.forcetower.uefs.core.injection.Injectable
+import com.forcetower.uefs.core.vm.UViewModelFactory
+import com.forcetower.uefs.feature.shared.extensions.provideActivityViewModel
+import javax.inject.Inject
 
-class AccountSettingsFragment : PreferenceFragmentCompat() {
+class AccountSettingsFragment : PreferenceFragmentCompat(), Injectable {
+    @Inject
+    lateinit var factory: UViewModelFactory
+    lateinit var viewModel: SettingsViewModel
+
+    private val listener = SharedPreferences.OnSharedPreferenceChangeListener { shared, key ->
+        onPreferenceChange(shared, key)
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_account, rootKey)
+        viewModel = provideActivityViewModel(factory)
     }
+
+    private fun onPreferenceChange(preference: SharedPreferences, key: String) {
+        when (key) {
+            "stg_choice_score" -> toggleScoreChoice(preference.getBoolean(key, false))
+        }
+    }
+
+    private fun toggleScoreChoice(calculated: Boolean) {
+        if (calculated) {
+            viewModel.getAllTheGrades()
+            Toast.makeText(requireContext(), R.string.all_the_grades_will_be_downloaded, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getSharedPreferences().registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener)
+    }
+
+    private fun getSharedPreferences() = preferenceManager.sharedPreferences
 }
