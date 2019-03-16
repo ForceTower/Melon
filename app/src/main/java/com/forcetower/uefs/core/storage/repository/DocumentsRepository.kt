ab58183950ebdbf4162e1f66c6d125c5af6220b4
João Paulo <joaopaulo761@gmail.com>
2019-03-16 18:49:19 -0300

    :zap: Documents can be requested even when login fails

--- app/src/main/java/com/forcetower/uefs/core/storage/repository/DocumentsRepository.kt
+++ app/src/main/java/com/forcetower/uefs/core/storage/repository/DocumentsRepository.kt
@@ -88,7 +88,7 @@ class DocumentsRepository @Inject constructor(
         } else {
             database.documentDao().updateDownloading(true, document.value)
             val login = SagresNavigator.instance.login(access.username, access.password)
-            if (login.status != Status.SUCCESS) {
+            if (login.status == Status.INVALID_LOGIN) {
                 Timber.d("Login failed. Login status is: ${login.status}")
                 data?.postValue(Resource.error("Login Failed", 800, Exception("Login Failed")))
             } else {
