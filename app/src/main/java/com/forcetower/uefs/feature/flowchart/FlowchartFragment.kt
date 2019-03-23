80f015baf435f87893452fde8f23ec58ca6467b0
João Paulo Sena <joaopaulo761@gmail.com>
2019-03-23 13:16:59 -0300

    :construction: Start of Flowchart layout

---
+++ app/src/main/java/com/forcetower/uefs/feature/flowchart/FlowchartFragment.kt
@@ -0,0 +1,14 @@
+package com.forcetower.uefs.feature.flowchart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.forcetower.uefs.feature.shared.UFragment
+
+class FlowchartFragment : UFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
