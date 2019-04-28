808dc9b46079e7f0e93c2d6171c64924754fa34d
João Paulo Sena <joaopaulo761@gmail.com>
2019-04-28 12:45:12 -0300

    :lipstick: Show confirmation always expanded

--- app/src/main/java/com/forcetower/uefs/feature/home/LogoutConfirmationFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/home/LogoutConfirmationFragment.kt
@@ -1,17 +1,22 @@
 package com.forcetower.uefs.feature.home
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.view.ContextThemeWrapper
 import com.forcetower.uefs.R
 import com.forcetower.uefs.core.injection.Injectable
 import com.forcetower.uefs.core.vm.UViewModelFactory
 import com.forcetower.uefs.databinding.FragmentLogoutConfirmationBinding
 import com.forcetower.uefs.feature.shared.extensions.provideActivityViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import javax.inject.Inject
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import timber.log.Timber
 
 class LogoutConfirmationFragment : BottomSheetDialogFragment(), Injectable {
     @Inject
@@ -29,5 +34,21 @@ class LogoutConfirmationFragment : BottomSheetDialogFragment(), Injectable {
             btnCancel.setOnClickListener { dismiss() }
             btnConfirm.setOnClickListener { viewModel.logout(); dismiss() }
         }.root
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val created = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+
+        created.setOnShowListener { dialog ->
+            val d = dialog as BottomSheetDialog
+
+            val bottomSheet = d.findViewById(com.google.android.material.R.id.design_bottom_sheet) as? FrameLayout
+            if (bottomSheet != null) {
+                BottomSheetBehavior.from(bottomSheet).state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                Timber.e("Bottom sheet dialog is null.. Theme will not apply")
+            }
+        }
+        return created
     }
 }
