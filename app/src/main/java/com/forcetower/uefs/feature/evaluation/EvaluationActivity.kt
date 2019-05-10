a5a365f0ffd981779a483c294b9d08ca68586572
João Paulo Sena <joaopaulo761@gmail.com>
2019-05-09 22:35:58 -0300

    :construction: Creates home screen

---
+++ app/src/main/java/com/forcetower/uefs/feature/evaluation/EvaluationActivity.kt
@@ -0,0 +1,12 @@
+package com.forcetower.uefs.feature.evaluation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EvaluationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_evaluation)
+    }
+}
