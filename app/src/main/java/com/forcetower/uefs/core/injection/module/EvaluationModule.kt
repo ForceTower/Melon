5aab23a63a1fef258a1d83a90e87c5985efded58
João Paulo Sena <joaopaulo761@gmail.com>
2019-05-14 21:51:08 -0300

    :rocket: Initial Fragment

---
+++ app/src/main/java/com/forcetower/uefs/core/injection/module/EvaluationModule.kt
@@ -0,0 +1,32 @@
+package com.forcetower.uefs.core.injection.module
+
+import com.forcetower.uefs.feature.disciplines.DisciplineFragment
+import com.forcetower.uefs.feature.evaluation.InitialFragment
+import com.forcetower.uefs.feature.evaluation.PresentationFragment
+import com.forcetower.uefs.feature.evaluation.home.HomeFragment
+import com.forcetower.uefs.feature.evaluation.rating.RatingFragment
+import com.forcetower.uefs.feature.evaluation.search.SearchFragment
+import com.forcetower.uefs.feature.evaluation.teacher.TeacherFragment
+import com.forcetower.uefs.feature.universe.UnesverseRequiredFragment
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class EvaluationModule {
+    @ContributesAndroidInjector
+    abstract fun initial(): InitialFragment
+    @ContributesAndroidInjector
+    abstract fun presentation(): PresentationFragment
+    @ContributesAndroidInjector
+    abstract fun unesverseRequired(): UnesverseRequiredFragment
+    @ContributesAndroidInjector
+    abstract fun home(): HomeFragment
+    @ContributesAndroidInjector
+    abstract fun search(): SearchFragment
+    @ContributesAndroidInjector
+    abstract fun teacher(): TeacherFragment
+    @ContributesAndroidInjector
+    abstract fun discipline(): DisciplineFragment
+    @ContributesAndroidInjector
+    abstract fun rating(): RatingFragment
+}
