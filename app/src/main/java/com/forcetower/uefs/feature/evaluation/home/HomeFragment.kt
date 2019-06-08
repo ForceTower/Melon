e321fda3843e50b30279247ca0959ea58ff640dd
ForceTower <joaopaulo761@gmail.com>
2019-06-08 12:20:08 -0300

    :rocket: Starts the Home Evaluation work

--- app/src/main/java/com/forcetower/uefs/feature/evaluation/home/HomeFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/evaluation/home/HomeFragment.kt
@@ -1,5 +1,19 @@
 package com.forcetower.uefs.feature.evaluation.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.forcetower.uefs.core.injection.Injectable
+import com.forcetower.uefs.databinding.FragmentEvaluationHomeBinding
 import com.forcetower.uefs.feature.shared.UFragment
 
-class HomeFragment : UFragment()
+class HomeFragment : UFragment(), Injectable {
+    private lateinit var binding: FragmentEvaluationHomeBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return FragmentEvaluationHomeBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
+    }
+}
