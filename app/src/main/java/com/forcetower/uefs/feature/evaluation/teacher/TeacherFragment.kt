5b07343b02a4e84ae6b7ff0f7fb93a3c0681a1a8
ForceTower <joaopaulo761@gmail.com>
2019-06-11 17:22:26 -0300

    :rocket: Revert animation

--- app/src/main/java/com/forcetower/uefs/feature/evaluation/teacher/TeacherFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/evaluation/teacher/TeacherFragment.kt
@@ -1,5 +1,19 @@
 package com.forcetower.uefs.feature.evaluation.teacher
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.forcetower.uefs.core.injection.Injectable
+import com.forcetower.uefs.core.vm.UViewModelFactory
 import com.forcetower.uefs.feature.shared.UFragment
+import javax.inject.Inject
 
-class TeacherFragment : UFragment()
+class TeacherFragment : UFragment(), Injectable {
+    @Inject
+    lateinit var factory: UViewModelFactory
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return null
+    }
+}
