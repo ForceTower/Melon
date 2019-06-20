df105ce95b9e1dcb209c60603cbebfc6425056f8
João Paulo <joaopaulo761@gmail.com>
2019-06-19 23:52:10 -0300

    x:rocket: Rating stuff

---
+++ app/src/main/java/com/forcetower/uefs/core/model/unes/Question.kt
@@ -0,0 +1,14 @@
+package com.forcetower.uefs.core.model.unes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Question(
+    @PrimaryKey(autoGenerate = false)
+    val id: Long,
+    val question: String,
+    val description: String?,
+    val teacher: Boolean,
+    val discipline: Boolean
+)
--- app/src/main/java/com/forcetower/uefs/feature/evaluation/rating/RatingDialog.kt
+++ app/src/main/java/com/forcetower/uefs/feature/evaluation/rating/RatingDialog.kt
@@ -1,3 +1,64 @@
 package com.forcetower.uefs.feature.evaluation.rating
 
-class RatingDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import com.forcetower.uefs.core.injection.Injectable
+import com.forcetower.uefs.core.model.unes.Question
+import com.forcetower.uefs.core.storage.resource.Resource
+import com.forcetower.uefs.core.vm.UViewModelFactory
+import com.forcetower.uefs.databinding.DialogEvaluationRatingBinding
+import com.forcetower.uefs.feature.evaluation.EvaluationViewModel
+import com.forcetower.uefs.feature.shared.FragmentAdapter
+import com.forcetower.uefs.feature.shared.UFragment
+import com.forcetower.uefs.feature.shared.extensions.provideActivityViewModel
+import javax.inject.Inject
+
+class RatingDialog : UFragment(), Injectable {
+    @Inject
+    lateinit var factory: UViewModelFactory
+    private lateinit var viewModel: EvaluationViewModel
+    private lateinit var binding: DialogEvaluationRatingBinding
+    private lateinit var adapter: FragmentAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewModel = provideActivityViewModel(factory)
+        adapter = FragmentAdapter(childFragmentManager)
+        return DialogEvaluationRatingBinding.inflate(inflater, container, false).also {
+            binding = it
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewPager.adapter = adapter
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.getQuestionsForTeacher().observe(this, Observer { useResponse(it) })
+    }
+
+    private fun useResponse(resource: Resource<List<Question>>) {
+        val data = resource.data
+        if (data != null) {
+            binding.groupLoading.visibility = GONE
+            binding.viewPager.visibility = VISIBLE
+
+            createFragmentsList(data)
+        } else {
+            // todo error handle
+            binding.groupLoading.visibility = VISIBLE
+            binding.viewPager.visibility = GONE
+        }
+    }
+
+    private fun createFragmentsList(data: List<Question>) {
+        val fragments = data.map { InternalQuestionFragment.newInstance(it) }
+        adapter.setItems(fragments)
+    }
+}
