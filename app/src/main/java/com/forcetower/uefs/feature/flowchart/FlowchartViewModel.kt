2186d1f0a673032179e8d3666e1e3808358674d9
João Paulo <joaopaulo761@gmail.com>
2019-06-29 16:09:28 -0300

    :clown_face: Mocking and creating flowchart fragments

--- app/src/main/java/com/forcetower/uefs/feature/flowchart/FlowchartViewModel.kt
+++ app/src/main/java/com/forcetower/uefs/feature/flowchart/FlowchartViewModel.kt
@@ -1,9 +1,32 @@
 package com.forcetower.uefs.feature.flowchart
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.forcetower.uefs.core.model.unes.FlowchartDisciplineUI
+import com.forcetower.uefs.core.model.unes.FlowchartSemesterUI
 import com.forcetower.uefs.core.storage.repository.FlowchartRepository
+import com.forcetower.uefs.core.vm.Event
+import com.forcetower.uefs.feature.flowchart.home.SemesterInteractor
+import com.forcetower.uefs.feature.flowchart.semester.DisciplineInteractor
 import javax.inject.Inject
 
 class FlowchartViewModel @Inject constructor(
     private val repository: FlowchartRepository
-) : ViewModel()
+) : ViewModel(), SemesterInteractor, DisciplineInteractor {
+    private val _onSemesterSelect = MutableLiveData<Event<FlowchartSemesterUI>>()
+    val onSemesterSelect: LiveData<Event<FlowchartSemesterUI>>
+        get() = _onSemesterSelect
+
+    private val _onDisciplineSelect = MutableLiveData<Event<FlowchartDisciplineUI>>()
+    val onDisciplineSelect: LiveData<Event<FlowchartDisciplineUI>>
+        get() = _onDisciplineSelect
+
+    override fun onSemesterSelected(semester: FlowchartSemesterUI) {
+        _onSemesterSelect.value = Event(semester)
+    }
+
+    override fun onDisciplineSelected(discipline: FlowchartDisciplineUI) {
+        _onDisciplineSelect.value = Event(discipline)
+    }
+}
