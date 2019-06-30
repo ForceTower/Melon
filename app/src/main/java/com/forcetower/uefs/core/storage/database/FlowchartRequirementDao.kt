ada280c8c50e81e2878cc220fb567dd985a4f364
João Paulo <joaopaulo761@gmail.com>
2019-06-30 16:52:44 -0300

    :sparkles: Information from flowchart comes from API

---
+++ app/src/main/java/com/forcetower/uefs/core/storage/database/FlowchartRequirementDao.kt
@@ -0,0 +1,12 @@
+package com.forcetower.uefs.core.storage.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import com.forcetower.uefs.core.model.unes.FlowchartRequirementUI
+
+@Dao
+interface FlowchartRequirementDao {
+    @Query("select FR.id as id, FR.type as type, D.name as shownName, FR.requiredDisciplineId as requiredDisciplineId, FR.coursePercentage as coursePercentage, FR.courseHours as courseHours from FlowchartRequirement FR inner join FlowchartDiscipline FD on FR.requiredDisciplineId = FD.id inner join Discipline D on FD.disciplineId = D.uid where fr.disciplineId = :disciplineId")
+    fun getDecoratedList(disciplineId: Long): LiveData<List<FlowchartRequirementUI>>
+}
---
+++ app/src/main/java/com/forcetower/uefs/core/storage/database/dao/FlowchartDao.kt
@@ -0,0 +1,53 @@
+package com.forcetower.uefs.core.storage.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Transaction
+import com.forcetower.uefs.core.model.service.FlowchartDTO
+import com.forcetower.uefs.core.model.unes.Discipline
+import com.forcetower.uefs.core.model.unes.Flowchart
+import com.forcetower.uefs.core.model.unes.FlowchartDiscipline
+import com.forcetower.uefs.core.model.unes.FlowchartRequirement
+import com.forcetower.uefs.core.model.unes.FlowchartSemester
+
+@Dao
+abstract class FlowchartDao {
+    @Transaction
+    open fun insertFromNetwork(data: FlowchartDTO) {
+        val flowchart = data.toFlowchart()
+        insertFlowchart(flowchart)
+
+        val semesters = data.semesters.map { it.toSemester() }
+        insertSemesters(semesters)
+
+        val requirements = mutableListOf<FlowchartRequirement>()
+
+        data.semesters.forEach { semester ->
+            semester.disciplines.forEach { discipline ->
+                val localDiscipline = discipline.toLocalDiscipline()
+                val localId = insertLocalDiscipline(localDiscipline)
+                val flowDiscipline = discipline.toDiscipline(localId, semester.id)
+                insertDiscipline(flowDiscipline)
+                requirements.addAll(discipline.requirements)
+            }
+        }
+
+        insertRequirements(requirements)
+    }
+
+    @Insert(onConflict = REPLACE)
+    protected abstract fun insertFlowchart(flowchart: Flowchart)
+
+    @Insert(onConflict = REPLACE)
+    protected abstract fun insertSemesters(semesters: List<FlowchartSemester>)
+
+    @Insert(onConflict = REPLACE)
+    protected abstract fun insertDiscipline(discipline: FlowchartDiscipline)
+
+    @Insert(onConflict = REPLACE)
+    protected abstract fun insertRequirements(requirements: List<FlowchartRequirement>)
+
+    @Insert(onConflict = REPLACE)
+    protected abstract fun insertLocalDiscipline(discipline: Discipline): Long
+}
