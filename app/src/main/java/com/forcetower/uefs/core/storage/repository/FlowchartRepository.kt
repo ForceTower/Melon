689bd60a4923a24a62170f6e6bd6a1f6f404d332
João Paulo <joaopaulo761@gmail.com>
2019-07-02 20:03:49 -0300

    :rocket: Requirements and Unlocks recursive

--- app/src/main/java/com/forcetower/uefs/core/storage/repository/FlowchartRepository.kt
+++ app/src/main/java/com/forcetower/uefs/core/storage/repository/FlowchartRepository.kt
@@ -1,9 +1,15 @@
 package com.forcetower.uefs.core.storage.repository
 
+import android.content.Context
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.crashlytics.android.Crashlytics
 import com.forcetower.uefs.AppExecutors
+import com.forcetower.uefs.R
 import com.forcetower.uefs.core.model.api.UResponse
 import com.forcetower.uefs.core.model.service.FlowchartDTO
+import com.forcetower.uefs.core.model.unes.FlowchartRequirementUI
 import com.forcetower.uefs.core.model.unes.FlowchartSemesterUI
 import com.forcetower.uefs.core.storage.database.UDatabase
 import com.forcetower.uefs.core.storage.network.UService
@@ -16,8 +22,12 @@ import javax.inject.Inject
 class FlowchartRepository @Inject constructor(
     private val database: UDatabase,
     private val service: UService,
-    private val executors: AppExecutors
+    private val executors: AppExecutors,
+    context: Context
 ) {
+    private val recursiveRequirements = context.getString(R.string.flowchart_recursive_continuation)
+    private val recursiveUnlocks = context.getString(R.string.flowchart_recursive_unlock)
+
     fun getFlowchart(courseId: Long): LiveData<Resource<List<FlowchartSemesterUI>>> {
         return object : NetworkBoundResource<List<FlowchartSemesterUI>, UResponse<FlowchartDTO>>(executors) {
             override fun loadFromDb() = database.flowchartSemesterDao().getDecoratedList(courseId)
@@ -35,4 +45,78 @@ class FlowchartRepository @Inject constructor(
     fun getDisciplineUI(disciplineId: Long) = database.flowchartDisciplineDao().getDecorated(disciplineId)
     fun getSemesterName(disciplineId: Long) = database.flowchartDisciplineDao().getSemesterFromDiscipline(disciplineId)
     fun getRequirementsUI(disciplineId: Long) = database.flowchartRequirementDao().getDecoratedList(disciplineId)
+    fun getRecursiveRequirementsUI(disciplineId: Long): LiveData<List<FlowchartRequirementUI>> {
+        val result = MutableLiveData<List<FlowchartRequirementUI>>()
+        executors.diskIO().execute {
+            try {
+                val everything = getRecursiveRequirementsWorker(disciplineId).sortedWith(Comparator { a, b ->
+                    val diff = compareValues(a.sequence, b.sequence)
+                    if (diff != 0) diff else compareValues(a.shownName, b.shownName)
+                })
+                result.postValue(everything)
+            } catch (t: Throwable) {
+                Timber.e(t, "A stack overflow!")
+                Crashlytics.log("Probably a stack overflow happened on requirements!!!")
+                Crashlytics.logException(t)
+            }
+        }
+        return result
+    }
+
+    fun getRecursiveUnlockRequirementUI(disciplineId: Long): LiveData<List<FlowchartRequirementUI>> {
+        val result = MutableLiveData<List<FlowchartRequirementUI>>()
+        executors.diskIO().execute {
+            try {
+                val everything = getRecursiveUnlockRequirementWorker(disciplineId).sortedWith(Comparator { a, b ->
+                    val diff = compareValues(a.sequence, b.sequence)
+                    if (diff != 0) diff else compareValues(a.shownName, b.shownName)
+                })
+                result.postValue(everything)
+            } catch (t: Throwable) {
+                Timber.e(t, "A stack overflow!")
+                Crashlytics.log("Probably a stack overflow happened on unlock!!!")
+                Crashlytics.logException(t)
+            }
+        }
+        return result
+    }
+
+    @WorkerThread
+    private fun getRecursiveRequirementsWorker(disciplineId: Long?): List<FlowchartRequirementUI> {
+        disciplineId ?: return emptyList()
+
+        val requirements = database.flowchartRequirementDao().getDecoratedListDirect(disciplineId).filter {
+            it.requiredDisciplineId != null && it.typeId == 1
+        }.map { FlowchartRequirementUI(it.id, recursiveRequirements, it.shownName, it.disciplineId, it.requiredDisciplineId, it.coursePercentage, it.courseHours, it.typeId, it.sequence) }
+        if (requirements.isEmpty()) return emptyList()
+
+        val iteration = mutableListOf<FlowchartRequirementUI>()
+        iteration += requirements
+
+        requirements.forEach {
+            val items = getRecursiveRequirementsWorker(it.requiredDisciplineId)
+            iteration += items
+        }
+
+        return iteration
+    }
+
+    private fun getRecursiveUnlockRequirementWorker(disciplineId: Long?): List<FlowchartRequirementUI> {
+        disciplineId ?: return emptyList()
+
+        val dependants = database.flowchartRequirementDao().getDecoratedDependenciesDirect(disciplineId).filter {
+            it.requiredDisciplineId != null && it.typeId == 1
+        }.map { FlowchartRequirementUI(it.id, recursiveUnlocks, it.shownName, it.disciplineId, it.requiredDisciplineId, it.coursePercentage, it.courseHours, it.typeId, it.sequence) }
+        if (dependants.isEmpty()) return emptyList()
+
+        val iteration = mutableListOf<FlowchartRequirementUI>()
+        iteration += dependants
+
+        dependants.forEach {
+            val items = getRecursiveUnlockRequirementWorker(it.disciplineId)
+            iteration += items
+        }
+
+        return iteration
+    }
 }
