108026c65925334275ac1e19819c5810da6f142b
João Paulo <joaopaulo761@gmail.com>
2019-07-02 20:15:52 -0300

    :green_heart: Fix emissing table join

--- app/src/main/java/com/forcetower/uefs/core/storage/database/FlowchartRequirementDao.kt
+++ app/src/main/java/com/forcetower/uefs/core/storage/database/FlowchartRequirementDao.kt
@@ -7,7 +7,7 @@ import com.forcetower.uefs.core.model.unes.FlowchartRequirementUI
 
 @Dao
 interface FlowchartRequirementDao {
-    @Query("select FR.id as id, FR.type as type, FR.disciplineId as disciplineId, FR.typeId as typeId, D.name as shownName, FR.requiredDisciplineId as requiredDisciplineId, FR.coursePercentage as coursePercentage, FR.courseHours as courseHours from FlowchartRequirement FR left join FlowchartDiscipline FD on FR.requiredDisciplineId = FD.id left join Discipline D on FD.disciplineId = D.uid where fr.disciplineId = :disciplineId")
+    @Query("select FR.id as id, FR.type as type, FR.disciplineId as disciplineId, FR.typeId as typeId, D.name as shownName, FR.requiredDisciplineId as requiredDisciplineId, FR.coursePercentage as coursePercentage, FR.courseHours as courseHours, FS.'order' as sequence from FlowchartRequirement FR left join FlowchartDiscipline FD on FR.requiredDisciplineId = FD.id left join Discipline D on FD.disciplineId = D.uid inner join FlowchartSemester FS on FD.semesterId = FS.id where fr.disciplineId = :disciplineId")
     fun getDecoratedList(disciplineId: Long): LiveData<List<FlowchartRequirementUI>>
 
     @Query("select FR.id as id, FR.type as type, FR.disciplineId as disciplineId, FR.typeId as typeId, D.name as shownName, FR.requiredDisciplineId as requiredDisciplineId, FR.coursePercentage as coursePercentage, FR.courseHours as courseHours, FS.'order' as sequence from FlowchartRequirement FR inner join FlowchartDiscipline FD on FR.requiredDisciplineId = FD.id inner join Discipline D on FD.disciplineId = D.uid inner join FlowchartSemester FS on FD.semesterId = FS.id where fr.disciplineId = :disciplineId")
