33d04217b407ca298e51101bacc9d24940f8e097
João Paulo <joaopaulo761@gmail.com>
2019-07-16 23:13:05 -0300

    :bug: Fixes a bug that affects Menininho
    Dropped index of disciplines names since PCC uses the same name on disciplines ;)

--- app/src/main/java/com/forcetower/uefs/core/storage/database/UDatabase.kt
+++ app/src/main/java/com/forcetower/uefs/core/storage/database/UDatabase.kt
@@ -128,7 +128,7 @@ import com.forcetower.uefs.core.util.Converters
     FlowchartDiscipline::class,
     FlowchartRequirement::class,
     ProfileStatement::class
-], version = 29, exportSchema = true)
+], version = 30, exportSchema = true)
 @TypeConverters(value = [Converters::class])
 abstract class UDatabase : RoomDatabase() {
     abstract fun accessDao(): AccessDao
