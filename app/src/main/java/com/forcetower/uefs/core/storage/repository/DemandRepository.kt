bcbef4eef81fffb3f8f91e528d6730b401b49802
João Paulo <joaopaulo761@gmail.com>
2019-07-16 21:38:50 -0300

    :rocket: Added some more analytics and shows the arrow up button on sheet

--- app/src/main/java/com/forcetower/uefs/core/storage/repository/DemandRepository.kt
+++ app/src/main/java/com/forcetower/uefs/core/storage/repository/DemandRepository.kt
@@ -29,8 +29,10 @@ package com.forcetower.uefs.core.storage.repository
 
 import android.content.Context
 import androidx.annotation.WorkerThread
+import androidx.core.os.bundleOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import com.crashlytics.android.Crashlytics
 import com.forcetower.sagres.SagresNavigator
 import com.forcetower.sagres.database.model.SDemandOffer
 import com.forcetower.sagres.operation.Status
@@ -40,6 +42,7 @@ import com.forcetower.uefs.core.storage.database.UDatabase
 import com.forcetower.uefs.core.storage.resource.Resource
 import com.forcetower.uefs.core.work.demand.CreateDemandWorker
 import com.forcetower.uefs.service.NotificationCreator
+import com.google.firebase.analytics.FirebaseAnalytics
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -48,7 +51,8 @@ import javax.inject.Singleton
 class DemandRepository @Inject constructor(
     private val database: UDatabase,
     private val executors: AppExecutors,
-    private val context: Context
+    private val context: Context,
+    private val analytics: FirebaseAnalytics
 ) {
 
     fun loadDemand(): LiveData<Resource<List<SDemandOffer>>> {
@@ -85,6 +89,8 @@ class DemandRepository @Inject constructor(
         return result
     }
 
+    fun getSelected() = database.demandOfferDao().getSelected()
+
     fun updateOfferSelection(offer: SDemandOffer, select: Boolean) {
         executors.diskIO().execute {
             database.demandOfferDao().updateOfferSelection(offer.uid, select)
@@ -97,25 +103,41 @@ class DemandRepository @Inject constructor(
 
     @WorkerThread
     fun executeCreateDemand() {
-        val list = database.demandOfferDao().getAllDirect()
-        val callback = SagresNavigator.instance.createDemandOffer(list)
+        try {
+            val list = database.demandOfferDao().getAllDirect()
+            val callback = SagresNavigator.instance.createDemandOffer(list)
 
-        val title = context.getString(R.string.demand_notification_title)
-        // TODO this must be a string resource
-        val content = when (callback.status) {
-            Status.STARTED -> "Começou a requisição e nem terminou"
-            Status.LOADING -> "Começou a carregar a requisição e não terminou"
-            Status.INVALID_LOGIN -> "Login falhou..."
-            Status.APPROVING -> "Erro desconhecido"
-            Status.NETWORK_ERROR -> "Erro de internet, a mensagem é: ${callback.throwable?.message}"
-            Status.RESPONSE_FAILED -> "Resposta falhou com código ${callback.code}"
-            Status.SUCCESS -> "Demanda completa! Observe o Sagres para ter certeza [Beta]"
-            Status.APPROVAL_ERROR -> "Algum erro ocorreu, provavelmente as matérias selecionadas não estão de acordo com as do servidor"
-            Status.GRADES_FAILED -> "Esse erro não deveria acontecer, mas aconteceu"
-            Status.UNKNOWN_FAILURE -> "Erro desconhecido, tudo que sei é: ${callback.message}"
-            Status.COMPLETED -> "A demanda foi enviada, mas o Sagres não respondeu com sucesso: ${callback.message}"
+            val title = context.getString(R.string.demand_notification_title)
+            // TODO this must be a string resource
+            val content = when (callback.status) {
+                Status.STARTED -> "Começou a requisição e nem terminou"
+                Status.LOADING -> "Começou a carregar a requisição e não terminou"
+                Status.INVALID_LOGIN -> "Login falhou..."
+                Status.APPROVING -> "Erro desconhecido"
+                Status.NETWORK_ERROR -> "Erro de internet, a mensagem é: ${callback.throwable?.message}"
+                Status.RESPONSE_FAILED -> "Resposta falhou com código ${callback.code}"
+                Status.SUCCESS -> "Demanda completa! Observe o Sagres para ter certeza [Beta]"
+                Status.APPROVAL_ERROR -> "Algum erro ocorreu, provavelmente as matérias selecionadas não estão de acordo com as do servidor"
+                Status.GRADES_FAILED -> "Esse erro não deveria acontecer, mas aconteceu"
+                Status.UNKNOWN_FAILURE -> "Erro desconhecido, tudo que sei é: ${callback.message}"
+                Status.COMPLETED -> "A demanda foi enviada, mas o Sagres não respondeu com sucesso, ele disse: ${callback.message}"
+            }
+
+            if (callback.status == Status.SUCCESS) {
+                analytics.logEvent("demand_user_completed_last_flow", null)
+            } else {
+                analytics.logEvent("demand_user_failed_last_flow", bundleOf(
+                    "status" to callback.status.name
+                ))
+            }
+
+            NotificationCreator.showSimpleNotification(context, title, content)
+        } catch (t: Throwable) {
+            NotificationCreator.showSimpleNotification(context, context.getString(R.string.demand_notification_title), "Uma exceção muito louca ocorreu: ${t.message}")
+            analytics.logEvent("demand_user_exception_at_worker", bundleOf(
+                "message" to t.message
+            ))
+            Crashlytics.logException(t)
         }
-
-        NotificationCreator.showSimpleNotification(context, title, content)
     }
 }
