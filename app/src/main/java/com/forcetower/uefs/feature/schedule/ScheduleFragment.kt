f6fa9318b9fcf211ee9c34461dfdf7383efa29a4
ForceTower <joaopaulo761@gmail.com>
2019-09-23 23:19:18 -0300

    :rotating_light: Remove unused imports

--- app/src/main/java/com/forcetower/uefs/feature/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/schedule/ScheduleFragment.kt
@@ -123,11 +123,11 @@ class ScheduleFragment : UFragment(), Injectable {
         if (TimeUtils.eventHasEnded()) {
             binding.btnSiecompSchedule.visibility = GONE
         } else {
-            //profileViewModel.getMeProfile().observe(this, Observer {
+            // profileViewModel.getMeProfile().observe(this, Observer {
                 binding.btnSiecompSchedule.show()
                 binding.btnSiecompSchedule.extend()
                 binding.btnSiecompSchedule.visibility = VISIBLE
-            //})
+            // })
         }
     }
 
