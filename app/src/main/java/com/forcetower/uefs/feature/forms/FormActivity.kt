91548ad1ca34cdb7fd1e5ead9623d65042e69150
João Paulo <joaopaulo761@gmail.com>
2019-10-20 13:38:25 -0300

    :rocket: Added in app questions

---
+++ app/src/main/java/com/forcetower/uefs/feature/forms/FormActivity.kt
@@ -0,0 +1,87 @@
+package com.forcetower.uefs.feature.forms
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.forcetower.uefs.R
+import com.forcetower.uefs.core.model.unes.Question
+import com.forcetower.uefs.core.vm.EventObserver
+import com.forcetower.uefs.core.vm.UViewModelFactory
+import com.forcetower.uefs.databinding.ActivityFormsBinding
+import com.forcetower.uefs.feature.shared.FragmentAdapter
+import com.forcetower.uefs.feature.shared.UActivity
+import com.forcetower.uefs.feature.shared.extensions.provideViewModel
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
+
+class FormActivity : UActivity(), HasSupportFragmentInjector {
+    @Inject
+    lateinit var factory: UViewModelFactory
+    @Inject
+    lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
+
+    private lateinit var binding: ActivityFormsBinding
+    private lateinit var viewModel: FormsViewModel
+    private lateinit var adapter: FragmentAdapter
+    private lateinit var currentData: List<Question>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_forms)
+        viewModel = provideViewModel(factory)
+        adapter = FragmentAdapter(supportFragmentManager)
+        binding.viewPager.adapter = adapter
+
+        val questions = createQuestions()
+        currentData = questions
+        createFragmentsList(questions)
+
+        viewModel.nextQuestion.observe(this, EventObserver {
+            val position = binding.viewPager.currentItem
+            val size = currentData.size
+            if (position + 1 >= size) {
+                viewModel.submitAnswers()
+                finish()
+            } else {
+                binding.viewPager.setCurrentItem(position + 1, true)
+            }
+        })
+    }
+
+    private fun createQuestions(): List<Question> {
+        return listOf(
+            Question(
+                0,
+                "O quão satisfeito você está com o Aplicativo UNES?",
+                "Onde 1 significa pouco satisfeito e 5 significa muito satisfeito",
+                teacher = false,
+                discipline = false,
+                formId = "entry.729050027"
+            ),
+            Question(
+                1,
+                "O quanto você está incomodado com a forma como o Aplicativo UNES exibe os anúncios atualmente?",
+                "Onde 1 significa não incomoda e 5 significa incomoda muito",
+                teacher = false,
+                discipline = false,
+                formId = "entry.611283944"
+            ),
+            Question(
+                2,
+                "O quanto você recomendaria o Aplicativo UNES para um colega?",
+                "Onde 1 significa de jeito nenhum e 5 com certeza recomendo",
+                teacher = false,
+                discipline = false,
+                formId = "entry.637152166"
+            )
+        )
+    }
+
+    private fun createFragmentsList(data: List<Question>) {
+        val fragments = data.map { InternalFormFragment.newInstance(it) }
+        adapter.setItems(fragments)
+    }
+
+    override fun supportFragmentInjector() = fragmentInjector
+}
