51234958296356a54541a792939d08d770fa1b71
João Paulo Sena <joaopaulo761@gmail.com>
2020-01-03 20:21:23 -0300

    :rocket: Add dashboard to nav graph

--- core/src/main/java/com/forcetower/core/adapters/ViewBindingAdapters.kt
+++ core/src/main/java/com/forcetower/core/adapters/ViewBindingAdapters.kt
@@ -30,6 +30,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.net.toUri
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.viewpager.widget.ViewPager
@@ -166,6 +168,37 @@ fun dateFromLong(view: TextView, value: Long?) {
     view.text = str
 }
 
+fun View.doOnApplyWindowInsets(f: (View, WindowInsetsCompat, ViewPaddingState) -> Unit) {
+    // Create a snapshot of the view's padding state
+    val paddingState = createStateForView(this)
+    ViewCompat.setOnApplyWindowInsetsListener(this) { v, insets ->
+        f(v, insets, paddingState)
+        insets
+    }
+    requestApplyInsetsWhenAttached()
+}
+
+/**
+ * Call [View.requestApplyInsets] in a safe away. If we're attached it calls it straight-away.
+ * If not it sets an [View.OnAttachStateChangeListener] and waits to be attached before calling
+ * [View.requestApplyInsets].
+ */
+fun View.requestApplyInsetsWhenAttached() {
+    if (isAttachedToWindow) {
+        requestApplyInsets()
+    } else {
+        addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
+            override fun onViewAttachedToWindow(v: View) {
+                v.requestApplyInsets()
+            }
+
+            override fun onViewDetachedFromWindow(v: View) = Unit
+        })
+    }
+}
+
+private fun createStateForView(view: View) = ViewPaddingState(view.paddingLeft, view.paddingTop, view.paddingRight, view.paddingBottom, view.paddingStart, view.paddingEnd)
+
 /**
  * An interface for responding to image loading completion.
  */
@@ -173,3 +206,12 @@ interface ImageLoadListener {
     fun onImageLoaded()
     fun onImageLoadFailed()
 }
+
+data class ViewPaddingState(
+    val left: Int,
+    val top: Int,
+    val right: Int,
+    val bottom: Int,
+    val start: Int,
+    val end: Int
+)
