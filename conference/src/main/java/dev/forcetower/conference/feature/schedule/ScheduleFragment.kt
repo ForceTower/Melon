29aaabb221fbc8fca1467a8df3921ac18c59c3ab
João Paulo Sena <joaopaulo761@gmail.com>
2020-02-08 15:20:10 -0300

    :rocket: Schedule begins

---
+++ conference/src/main/java/dev/forcetower/conference/feature/schedule/ScheduleFragment.kt
@@ -0,0 +1,74 @@
+/*
+ * This file is part of the UNES Open Source Project.
+ * UNES is licensed under the GNU GPLv3.
+ *
+ * Copyright (c) 2020. João Paulo Sena <joaopaulo761@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.forcetower.conference.feature.schedule
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.forcetower.uefs.feature.shared.UFragment
+import dev.forcetower.conference.core.model.general.DayIndicator
+import dev.forcetower.conference.core.model.persistence.ConferenceDay
+import dev.forcetower.conference.databinding.FragmentScheduleBinding
+import org.threeten.bp.ZonedDateTime
+import timber.log.Timber
+
+class ScheduleFragment : UFragment() {
+    private lateinit var binding: FragmentScheduleBinding
+    private lateinit var dayAdapter: DayAdapter
+    private val viewModel by viewModels<ScheduleViewModel>()
+
+    private var cachedBubbleRange: IntRange? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return FragmentScheduleBinding.inflate(inflater, container, false).also {
+            binding = it
+            dayAdapter = DayAdapter(viewModel, this)
+            binding.dayIndicators.adapter = dayAdapter
+        }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buildDayIndicators(days)
+    }
+
+    private fun buildDayIndicators(days: List<ConferenceDay>) {
+//        val bubbleRange = cachedBubbleRange ?: return
+        val mapped = days.mapIndexed { index: Int, day: ConferenceDay ->
+            DayIndicator(day = day, checked = false)
+        }
+
+        Timber.d("Setting $mapped")
+        dayAdapter.submitList(mapped)
+    }
+
+    companion object {
+        val days = listOf(
+            ConferenceDay(ZonedDateTime.now(), ZonedDateTime.now().plusHours(3)),
+            ConferenceDay(ZonedDateTime.now().plusDays(1), ZonedDateTime.now().plusDays(1).plusHours(3)),
+            ConferenceDay(ZonedDateTime.now().plusDays(2), ZonedDateTime.now().plusDays(2).plusHours(3)),
+            ConferenceDay(ZonedDateTime.now().plusDays(3), ZonedDateTime.now().plusDays(3).plusHours(3)),
+            ConferenceDay(ZonedDateTime.now().plusDays(4), ZonedDateTime.now().plusDays(4).plusHours(3))
+        )
+    }
+}
