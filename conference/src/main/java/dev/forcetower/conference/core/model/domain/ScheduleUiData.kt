b5e14f198ee701eedc7b431b5b30f37e3cb9baa1
João Paulo Sena <joaopaulo761@gmail.com>
2020-02-09 12:41:19 -0300

    :construction: Creating schedule session item

--- conference/src/main/java/dev/forcetower/conference/core/model/domain/ScheduleUiData.kt
+++ conference/src/main/java/dev/forcetower/conference/core/model/domain/ScheduleUiData.kt
@@ -18,14 +18,11 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package dev.forcetower.conference.core.bindings
+package dev.forcetower.conference.core.model.domain
 
-import android.widget.TextView
-import androidx.databinding.BindingAdapter
-import dev.forcetower.conference.core.model.domain.DayIndicator
-import dev.forcetower.conference.core.utils.TimeUtils
+import dev.forcetower.conference.core.model.persistence.Session
 
-@BindingAdapter("indicatorText")
-fun setIndicatorText(view: TextView, dayIndicator: DayIndicator) {
-    view.text = TimeUtils.getShortNameDay(dayIndicator.day)
-}
+data class ScheduleUiData(
+    val sessions: List<Session>,
+    val indexed: ConferenceDayIndexed
+)
