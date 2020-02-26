7383c09264baa713b1529d60718bd0d5ec3bf36e
João Paulo Sena <joaopaulo761@gmail.com>
2020-02-26 12:19:00 -0300

    :rocket: Bad Devide information

--- app/src/main/java/com/forcetower/uefs/feature/settings/AdvancedSettingsFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/settings/AdvancedSettingsFragment.kt
@@ -20,12 +20,16 @@
 
 package com.forcetower.uefs.feature.settings
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
+import android.provider.Settings
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
@@ -37,10 +41,12 @@ import com.forcetower.uefs.R
 import com.forcetower.uefs.core.util.VersionUtils
 import com.forcetower.uefs.core.util.isStudentFromUEFS
 import com.forcetower.uefs.core.vm.UViewModelFactory
+import com.forcetower.uefs.core.work.sync.SyncMainWorker
 import com.forcetower.uefs.feature.messages.MessagesDFMViewModel
 import com.forcetower.uefs.feature.web.CustomTabActivityHelper
 import com.google.android.material.snackbar.Snackbar
 import com.judemanutd.autostarter.AutoStartPermissionHelper
+import timber.log.Timber
 import java.util.Locale
 import javax.inject.Inject
 
@@ -55,13 +61,7 @@ class AdvancedSettingsFragment : PreferenceFragmentCompat(), Injectable {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_advanced, rootKey)
-        if (VersionUtils.isMarshmallow()) {
-            findPreference<SwitchPreference>("stg_advanced_ignore_doze")?.let {
-                val pm = context?.getSystemService(Context.POWER_SERVICE) as PowerManager?
-                val ignoring = pm?.isIgnoringBatteryOptimizations(requireContext().packageName) ?: false
-                it.isChecked = ignoring
-            }
-        }
+        updateDozePreferences()
 
         if (!getSharedPreferences().isStudentFromUEFS()) {
             findPreference<SwitchPreference>("stg_advanced_aeri_tab")?.isVisible = false
@@ -91,9 +91,25 @@ class AdvancedSettingsFragment : PreferenceFragmentCompat(), Injectable {
         }
     }
 
+    private fun updateDozePreferences() {
+        if (VersionUtils.isMarshmallow()) {
+            findPreference<SwitchPreference>("stg_advanced_ignore_doze")?.let {
+                val pm = context?.getSystemService(Context.POWER_SERVICE) as PowerManager?
+                val ignoring = pm?.isIgnoringBatteryOptimizations(requireContext().packageName) ?: false
+                it.isChecked = ignoring
+            }
+        } else {
+            findPreference<SwitchPreference>("stg_advanced_ignore_doze")?.let {
+                it.isEnabled = false
+                it.setSummary(R.string.settings_adv_doze_mode_info_disabled)
+            }
+        }
+    }
+
     private fun onPreferenceChange(preference: SharedPreferences, key: String) {
         when (key) {
             "stg_advanced_aeri_tab" -> aeriTabs(preference.getBoolean(key, true))
+            "stg_advanced_ignore_doze" -> ignoreDoze(preference.getBoolean(key, false))
         }
     }
 
@@ -103,29 +119,35 @@ class AdvancedSettingsFragment : PreferenceFragmentCompat(), Injectable {
         }
     }
 
-//    private fun ignoreDoze(ignore: Boolean) {
-//        if (!ignore) {
-//            view?.let {
-//                Snackbar.make(it, "Para mudar, vá até Configurações > Apps e notificações no seu celular", Snackbar.LENGTH_LONG).show()
-//            }
-//        } else {
-//            if (VersionUtils.isMarshmallow()) {
-//                val pm = context?.getSystemService(Context.POWER_SERVICE) as PowerManager?
-//                val ignoring = pm?.isIgnoringBatteryOptimizations(context!!.packageName) ?: false
-//                Timber.d("Ignoring? $ignoring")
-//                if (!ignoring) {
-//                    val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS).apply {
-//                        data = Uri.parse("package:${context?.packageName}")
-//                    }
-//                    startActivity(intent)
-//                }
-//            }
-//        }
-//    }
+    @SuppressLint("BatteryLife")
+    private fun ignoreDoze(ignore: Boolean) {
+        if (!VersionUtils.isMarshmallow()) return
+        if (!ignore) {
+            val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
+            startActivity(intent)
+        } else {
+            val pm = context?.getSystemService(Context.POWER_SERVICE) as PowerManager?
+            val ignoring = pm?.isIgnoringBatteryOptimizations(requireContext().packageName) ?: false
+            Timber.d("Ignoring? $ignoring")
+            if (!ignoring) {
+                val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS).apply {
+                    data = Uri.parse("package:${context?.packageName}")
+                }
+                startActivityForResult(intent, REQUEST_IGNORE_DOZE)
+            }
+        }
+    }
+
+    private fun setDozePreference(enabled: Boolean) {
+        findPreference<SwitchPreference>("stg_advanced_ignore_doze")?.let {
+            it.isChecked = enabled
+        }
+    }
 
     override fun onResume() {
         super.onResume()
         getSharedPreferences().registerOnSharedPreferenceChangeListener(listener)
+        updateDozePreferences()
     }
 
     override fun onPause() {
@@ -134,4 +156,27 @@ class AdvancedSettingsFragment : PreferenceFragmentCompat(), Injectable {
     }
 
     private fun getSharedPreferences() = preferenceManager.sharedPreferences
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_IGNORE_DOZE -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    SyncMainWorker.stopWorker(requireContext())
+                    SyncMainWorker.createWorker(requireContext(), 60)
+                    Snackbar.make(requireView(), getString(R.string.settings_accept_ignore_doze), Snackbar.LENGTH_LONG).show()
+                    getSharedPreferences().edit().putString("stg_sync_frequency", "60").apply()
+                    setDozePreference(true)
+                } else {
+                    Snackbar.make(requireView(), getString(R.string.settings_refuse_ignore_doze), Snackbar.LENGTH_LONG).show()
+                    getSharedPreferences().edit().putBoolean("stg_advanced_ignore_doze", false).apply()
+                    setDozePreference(false)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_IGNORE_DOZE = 40000
+    }
 }
