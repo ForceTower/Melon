f343167bd6d5368fcf27241544e603a2849eec24
João Paulo Sena <joaopaulo761@gmail.com>
2020-04-01 22:44:59 -0300

    :fire: This is a funny patch...

--- app/src/main/java/com/forcetower/uefs/core/constants/Constants.kt
+++ app/src/main/java/com/forcetower/uefs/core/constants/Constants.kt
@@ -22,9 +22,9 @@ package com.forcetower.uefs.core.constants
 
 object Constants {
     const val SELECTED_INSTITUTION_KEY = "selected_institution_worker"
-    const val UNES_SERVICE_BASE_URL = "unes.forcetower.dev"
+    const val UNES_SERVICE_BASE_URL = "unes-env.frarj7zp5x.us-east-2.elasticbeanstalk.com"
     private const val UNES_SERVICE_BASE_UPDATE = "unes.herokuapp.com"
-    const val UNES_SERVICE_URL = "https://$UNES_SERVICE_BASE_URL/api/"
+    const val UNES_SERVICE_URL = "http://$UNES_SERVICE_BASE_URL/api/"
     const val UNES_SERVICE_UPDATE = "http://$UNES_SERVICE_BASE_UPDATE/api/"
 
     const val DEVELOPER_EMAIL = "joaopaulo761@gmail.com"
