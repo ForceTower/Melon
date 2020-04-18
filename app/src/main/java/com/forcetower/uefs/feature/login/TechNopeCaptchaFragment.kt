334264cbc441aa122fe3a1ca10af9e471695e864
João Paulo Sena <joaopaulo761@gmail.com>
2020-04-18 12:36:17 -0300

    :rocket: Thunder

--- app/src/main/java/com/forcetower/uefs/feature/login/TechNopeCaptchaFragment.kt
+++ app/src/main/java/com/forcetower/uefs/feature/login/TechNopeCaptchaFragment.kt
@@ -24,9 +24,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.CookieManager
+import android.webkit.ConsoleMessage
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.Keep
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -36,9 +39,9 @@ import com.forcetower.uefs.core.vm.UViewModelFactory
 import com.forcetower.uefs.databinding.FragmentTechNopeCaptchaBinding
 import com.forcetower.uefs.feature.shared.UFragment
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_event_editor_index.password
 import timber.log.Timber
 import javax.inject.Inject
+
 
 class TechNopeCaptchaFragment : UFragment(), Injectable {
     @Inject
@@ -48,10 +51,6 @@ class TechNopeCaptchaFragment : UFragment(), Injectable {
     private val args by navArgs<TechNopeCaptchaFragmentArgs>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // create a layout that is basically a webview, monitor page progress and redirect.
-        // catch the cookies and set to the navigator
-        // import okhttp3.Cookie;
-        // SagresNavigator.instance.setCookies(List<Cookie>) ...... this method will be available on next version of navigator
         return FragmentTechNopeCaptchaBinding.inflate(inflater, container, false).also {
             binding = it
         }.root
@@ -61,44 +60,84 @@ class TechNopeCaptchaFragment : UFragment(), Injectable {
         super.onActivityCreated(savedInstanceState)
         showSnack("Eu odeio essa parte...", Snackbar.LENGTH_LONG)
         binding.webView.apply {
-            loadUrl("http://academico2.uefs.br/Portal/Acesso.aspx")
+            loadDataWithBaseURL("http://academico2.uefs.br", data, "text/html; charset=utf-8", "UTF-8", null)
             settings.apply {
                 javaScriptEnabled = true
                 userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
                 loadWithOverviewMode = true
-                useWideViewPort = true
+                // useWideViewPort = true
                 setSupportZoom(true)
                 builtInZoomControls = true
                 displayZoomControls = false
             }
             scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
             isScrollbarFadingEnabled = false
-            webViewClient = object : WebViewClient() {
-                override fun onPageFinished(view: WebView?, url: String?) {
-                    super.onPageFinished(view, url)
-                    val cookies = CookieManager.getInstance().getCookie(url)
-                    Timber.d("The URL: $url")
-                    Timber.d("The cookies $cookies")
-
-                    // It's hardcoded... yes
-                    if (url == "http://academico2.uefs.br/Portal/Modules/Portal/Default.aspx") {
-                        onCompleteSteps(cookies)
-                    }
-                }
-            }
+            addJavascriptInterface(this@TechNopeCaptchaFragment, "BridgeWebView");
         }
     }
 
-    private fun onCompleteSteps(cookies: String) {
-        // set cookies on navigator!
-        SagresNavigator.instance.setCookiesOnClient(cookies)
-        // Change to livedata event this is temp
-        if (lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
-            binding.webView.destroy()
-            val direction = TechNopeCaptchaFragmentDirections.actionLoginTechNopeToLoginSigningIn(args.username, args.password).apply {
-                skipLogin = true
-            }
-            findNavController().navigate(direction)
+    @Keep
+    @JavascriptInterface
+    fun reCaptchaCallback(token: String) {
+        Timber.d("reCaptcha token $token")
+        val directions = TechNopeCaptchaFragmentDirections.actionLoginTechNopeToLoginSigningIn(args.username, args.password).apply {
+            captchaToken = token
         }
+        findNavController().navigate(directions)
+    }
+
+    companion object {
+        private const val data = "<html>\n" +
+            "    <head>\n" +
+            "      <title>Shit</title>\n" +
+            "      <meta content='width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=0' name='viewport' />\n" +
+            "      <script src=\"https://www.google.com/recaptcha/api.js\" async defer></script>\n" +
+            "<script type=\"text/javascript\">\n" +
+            "    function captchaResponse(token){\n" +
+            "        BridgeWebView.reCaptchaCallback(token);\n" +
+            "    }\n" +
+            "</script>" +
+            "      <script>\n" +
+            "        function onSubmit() {\n" +
+            "          var response = grecaptcha.getResponse();\n" +
+            "          console.log(response)\n" +
+            "          window.postMessage(response);\n" +
+            "        }\n" +
+            "      </script>\n" +
+            "      <script>\n" +
+            "        window.onload = function (e) {\n" +
+            "          grecaptcha.execute()\n" +
+            "        }\n" +
+            "      </script>\n" +
+            "      <style>\n" +
+            "        html, body {\n" +
+            "          max-width: 100%;\n" +
+            "          overflow-x: hidden;\n" +
+            "        }\n" +
+            "        .status-message {\n" +
+            "          background-color: #00000000;\n" +
+            "          margin-bottom: 10px;\n" +
+            "          text-align: center;\n" +
+            "        }\n" +
+            "        textarea {\n" +
+            "          margin: 10px 0;\n" +
+            "          resize: none;\n" +
+            "        }\n" +
+            "        .g-recaptcha {\n" +
+            "          transform:scale(1.2);\n" +
+            "          transform-origin:0 0;\n" +
+            "        }\n" +
+            "        .hide-all {\n" +
+            "          display: none;\n" +
+            "        }\n" +
+            "      </style>\n" +
+            "    </head>\n" +
+            "    <body>\n" +
+            "        <div class=\"g-recaptcha\"\n" +
+            "          data-sitekey=\"6Lc5M-UUAAAAAOFIqIdUEP2BeaqFi3f-71HscRlB\"\n" +
+            "          data-callback=\"captchaResponse\">\n" +
+            "        </div>\n" +
+            "    </body>\n" +
+            "</html>"
     }
 }
