82e12ff3415085a8989812add3e5a50a45800493
João Paulo Sena <joaopaulo761@gmail.com>
2020-05-04 20:21:40 -0300

    :rocket: Stasis effect

--- app/src/main/java/com/forcetower/uefs/feature/home/HomeActivity.kt
+++ app/src/main/java/com/forcetower/uefs/feature/home/HomeActivity.kt
@@ -44,6 +44,7 @@ import com.forcetower.uefs.BuildConfig
 import com.forcetower.uefs.R
 import com.forcetower.uefs.REQUEST_IN_APP_UPDATE
 import com.forcetower.uefs.architecture.service.bigtray.BigTrayService
+import com.forcetower.uefs.architecture.service.sync.SyncService
 import com.forcetower.uefs.core.model.unes.Access
 import com.forcetower.uefs.core.model.unes.Account
 import com.forcetower.uefs.core.util.VersionUtils
@@ -166,6 +167,8 @@ class HomeActivity : UGameActivity(), HasAndroidInjector {
             viewModel.account.observe(this, Observer { Unit })
             checkServerAchievements()
             viewModel.getAffinityQuestions()
+            val intent = Intent(this, SyncService::class.java)
+            startService(intent)
         } catch (t: Throwable) {}
         moveToTask()
         // satisfactionSurvey()
