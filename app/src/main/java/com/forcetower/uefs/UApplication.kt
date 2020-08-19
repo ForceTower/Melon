27d4df8047ddd7aced48f6f711f85b35566f3d2e
João Sena <joaopaulo761@gmail.com>
2020-08-19 14:51:43 -0300

    :rocket: Use Crashlytics...

--- app/src/main/java/com/forcetower/uefs/UApplication.kt
+++ app/src/main/java/com/forcetower/uefs/UApplication.kt
@@ -69,13 +69,13 @@ class UApplication : Application(), HasAndroidInjector {
             return
         }
 
-//        if (BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG) {
             // Se em debug, print no logcat todas as informações
             Timber.plant(Timber.DebugTree())
-//        } else {
-//            // Em release, enviar exceptions para o crashlytics
-//            Timber.plant(CrashlyticsTree())
-//        }
+        } else {
+            // Em release, enviar exceptions para o crashlytics
+            Timber.plant(CrashlyticsTree())
+        }
         // Injeta as dependências. Este é o ponto inicial
         injectApplicationIfNecessary()
         super.onCreate()
