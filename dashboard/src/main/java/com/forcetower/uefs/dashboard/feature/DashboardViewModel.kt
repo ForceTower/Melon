b6eadd0ebd5526991d55e3c45a5d4f65f90fba0f
João Sena <joaopaulo761@gmail.com>
2020-09-19 19:30:06 -0300

    Upgrade dependencies

--- dashboard/src/main/java/com/forcetower/uefs/dashboard/feature/DashboardViewModel.kt
+++ dashboard/src/main/java/com/forcetower/uefs/dashboard/feature/DashboardViewModel.kt
@@ -35,6 +35,7 @@ import com.forcetower.uefs.feature.shared.TimeLiveData
 import timber.log.Timber
 import java.util.Calendar
 import javax.inject.Inject
+import kotlin.coroutines.suspendCoroutine
 
 class DashboardViewModel @Inject constructor(
     private val repository: DashboardRepository,
@@ -91,6 +92,9 @@ class DashboardViewModel @Inject constructor(
 
     fun onShowAllClasses() {
         _onMoveToSchedule.value = Event(Unit)
+    }
+
+    suspend fun interesting(): Int = suspendCoroutine { continuation ->
     }
 
     override fun onSelect(questionId: Long, student: SStudent) {
