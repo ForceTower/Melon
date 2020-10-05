711b6eee2878ded6387dd782c5687f034e9e8d90
João Sena <joaopaulo761@gmail.com>
2020-10-05 14:51:07 -0300

    Bring back UServices for disconnected accounts

--- app/src/main/java/com/forcetower/uefs/core/vm/UnesverseViewModel.kt
+++ app/src/main/java/com/forcetower/uefs/core/vm/UnesverseViewModel.kt
@@ -25,13 +25,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.forcetower.uefs.R
 import com.forcetower.uefs.core.model.unes.AccessToken
 import com.forcetower.uefs.core.storage.repository.cloud.AuthRepository
 import com.forcetower.uefs.core.storage.resource.Resource
-import com.forcetower.uefs.core.storage.resource.Status
 import com.forcetower.uefs.feature.shared.extensions.setValueIfNew
-import timber.log.Timber
+import kotlinx.coroutines.launch
 
 class UnesverseViewModel @ViewModelInject constructor(
     private val auth: AuthRepository
@@ -53,25 +53,15 @@ class UnesverseViewModel @ViewModelInject constructor(
     fun login() {
         if (_isLoggingIn.value == true) return
         _isLoggingIn.value = true
-        val source = auth.autoLogin()
-        _loggingIn.addSource(source) {
-            _loggingIn.value = it
-            Timber.d("The current login status: ${it.status}")
-            when {
-                it.status === Status.LOADING -> {
-                    _isLoggingIn.value = true
-                }
-                it.status === Status.ERROR -> {
-                    _loggingIn.removeSource(source)
-                    _loginMessenger.setValueIfNew(Event(R.string.failed_to_connect_to_unesverse))
-                    _isLoggingIn.value = false
-                }
-                else -> {
-                    _loggingIn.removeSource(source)
-                    _loginMessenger.setValueIfNew(Event(R.string.connected_to_the_unesverse))
-                    _isLoggingIn.value = false
-                }
+
+        viewModelScope.launch {
+            val token = auth.loginToService()
+            if (token != null) {
+                _loginMessenger.setValueIfNew(Event(R.string.connected_to_the_unesverse))
+            } else {
+                _loginMessenger.setValueIfNew(Event(R.string.failed_to_connect_to_unesverse))
             }
+            _isLoggingIn.value = false
         }
     }
 }
