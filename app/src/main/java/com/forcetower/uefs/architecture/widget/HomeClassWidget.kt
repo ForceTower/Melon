e10e31dc9448069987c90c30b01797b04bac86fa
João Sena <joaopaulo761@gmail.com>
2021-02-19 23:26:54 -0300

    Create a widget skeleton
    This was proposed at #16, and is currently under development

---
+++ app/src/main/java/com/forcetower/uefs/architecture/widget/HomeClassWidget.kt
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the UNES Open Source Project.
+ * UNES is licensed under the GNU GPLv3.
+ *
+ * Copyright (c) 2021. João Paulo Sena <joaopaulo761@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.forcetower.uefs.architecture.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import com.forcetower.uefs.R
+
+class HomeClassWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+        val views = RemoteViews(context.packageName, R.layout.widget_class_home)
+
+        appWidgetIds.forEach {
+            appWidgetManager.updateAppWidget(it, views)
+        }
+    }
+}
