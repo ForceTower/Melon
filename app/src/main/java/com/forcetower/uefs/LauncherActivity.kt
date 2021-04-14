10c6db760093d4e7d2391faf64914274b10bb120
João Sena <joaopaulo761@gmail.com>
2021-04-13 21:12:02 -0300

    Adjust architecture for SplashScreen flow

--- app/src/main/java/com/forcetower/uefs/LauncherActivity.kt
+++ app/src/main/java/com/forcetower/uefs/LauncherActivity.kt
@@ -21,19 +21,13 @@
 package com.forcetower.uefs
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.forcetower.core.lifecycle.EventObserver
-import com.forcetower.uefs.core.vm.Destination
-import com.forcetower.uefs.core.vm.LaunchViewModel
 import com.forcetower.uefs.feature.home.HomeActivity
 import com.forcetower.uefs.feature.login.LoginActivity
-import com.forcetower.uefs.service.NotificationCreator
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 /**
  * A atividade inicial do aplicativo, ela tem o papel de decidir qual tela mostrar
@@ -42,42 +36,20 @@ import javax.inject.Inject
  */
 @AndroidEntryPoint
 class LauncherActivity : AppCompatActivity() {
-    @Inject
-    lateinit var remoteConfig: FirebaseRemoteConfig
-    @Inject
-    lateinit var preferences: SharedPreferences
-
     private val viewModel by viewModels<LaunchViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState != null) return
-        createNewVersionNotification()
 
-        viewModel.direction.observe(
-            this,
-            EventObserver {
-                if (!viewModel.started) {
-                    when (it) {
-                        Destination.LOGIN_ACTIVITY -> startActivity(Intent(this, LoginActivity::class.java))
-                        Destination.HOME_ACTIVITY -> startActivity(Intent(this, HomeActivity::class.java))
-                    }
-                    viewModel.started = true
-                    finish()
-                }
+        viewModel.checkNewAppVersion()
+        viewModel.findStarterDirection()
+        viewModel.direction.observe(this, EventObserver { destination ->
+            when (destination) {
+                LaunchViewModel.Destination.LOGIN_ACTIVITY -> startActivity(Intent(this, LoginActivity::class.java))
+                LaunchViewModel.Destination.HOME_ACTIVITY -> startActivity(Intent(this, HomeActivity::class.java))
             }
-        )
-    }
-
-    private fun createNewVersionNotification() {
-        val currentVersion = remoteConfig.getLong("version_current")
-        val notified = preferences.getBoolean("version_ntf_key_$currentVersion", false)
-        if (currentVersion > BuildConfig.VERSION_CODE && !notified) {
-            val notes = remoteConfig.getString("version_notes")
-            val version = remoteConfig.getString("version_name")
-            val title = getString(R.string.new_version_ntf_title_format, version)
-            NotificationCreator.showSimpleNotification(this, title, notes)
-            preferences.edit().putBoolean("version_ntf_key_$currentVersion", true).apply()
-        }
+            finish()
+        })
     }
 }
