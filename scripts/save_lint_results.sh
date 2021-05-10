77cdf27675e444700129a3d571f05d5c488e1a07
João Sena <joaopaulo761@gmail.com>
2021-05-10 14:06:41 -0300

    Save lint results

--- scripts/save_lint_results.sh
+++ scripts/save_lint_results.sh
@@ -3,11 +3,11 @@
 RESULTS_DIR="ci_results/lint/"
 mkdir -p $RESULTS_DIR
 
-LINT_FILES=`find . -type f -regex ".*/build/reports/lint-results\..*" | grep -v third_party`
+LINT_FILES=$(find . -type f -regex ".*/build/reports/lint-results\..*" | grep -v third_party)
 
 for file in $LINT_FILES
 do
-  rename 's/\/(.*)\/build\/reports\/lint-results/\/$1\/build\/reports\/lint-results-$1/' $file
+  rename 's/\/(.*)\/build\/reports\/lint-results/\/$1\/build\/reports\/lint-results-$1/' "$file"
 done
 
 find . -type f -regex ".*/build/reports/lint-results-.*\..*" -exec cp {} $RESULTS_DIR \;
