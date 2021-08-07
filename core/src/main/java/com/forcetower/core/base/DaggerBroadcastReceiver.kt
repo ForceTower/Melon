26de4aca989ceefc80f7de59c750bd16b4f6ea14
João Sena <joaopaulo761@gmail.com>
2021-08-07 20:01:07 -0300

    Removed project

--- core/src/main/java/com/forcetower/core/base/DaggerBroadcastReceiver.kt
+++
@@ -1,9 +0,0 @@
-package com.forcetower.core.base
-
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-
-abstract class DaggerBroadcastReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {}
-}
--- decrypt_files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gpg --quiet --batch --yes --decrypt --passphrase="$SERVICES_PASSWORD" --output app/google-services.json app/google-services.json.gpg
