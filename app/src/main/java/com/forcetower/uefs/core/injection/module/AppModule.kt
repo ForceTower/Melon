af5199a3ae7d6cea6f6db10035b53f46568f338e
João Paulo Sena <joaopaulo761@gmail.com>
2024-04-18 21:35:26 -0300

    Update user agent

--- app/src/main/java/com/forcetower/uefs/core/injection/module/AppModule.kt
+++ app/src/main/java/com/forcetower/uefs/core/injection/module/AppModule.kt
@@ -110,7 +110,7 @@ object AppModule {
         } catch (error: Throwable) {
             Timber.w("Failed to obtain device UserAgent")
             Timber.w("UserAgent error ${error.message}")
-            "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/84.0.4147.125 Mobile Safari/537.36"
+            "Mozilla/5.0 (Linux; Android 10; MI 9 Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/123.0.6312.118 Mobile Safari/537.36"
         }
     }
 }
