00b7550a17e53ce99378d87790197d8bdc8e3031
João Paulo Sena <joaopaulo761@gmail.com>
2024-08-13 18:56:00 -0300

    No longer adds different memes to different branches

--- buildSrc/src/main/kotlin/dev/forcetower/gradle/utils/ProjectExtensions.kt
+++ buildSrc/src/main/kotlin/dev/forcetower/gradle/utils/ProjectExtensions.kt
@@ -14,19 +14,16 @@ fun Project.buildVersion(): Pair<Int, String> {
         val (majorStr, minorStr, patchStr) = lastTag.lowercase().replace('-', '.').split('.')
 
         var postfix = "main"
-        var adder = 20000
         if (branch == "development") {
-            adder = 30000
-            postfix = "beta"
+            postfix = "lazer"
         } else if (branch.startsWith("feature/")) {
-            adder = 30000
             postfix = branch.replace("feature/", "")
         }
 
         val (major, minor, patch) = listOf(majorStr, minorStr, patchStr.replace(Regex("[^0-9]"), "")).map { it.toInt() }
 
         val name = "${major}.${minor}.${patch}.${commitCount}.rev-${postfix} [${lastCommit}]"
-        val code = (21 * 100000) + commitCount + adder
+        val code = (21 * 100000) + commitCount + 30000
         return code to name
     } catch (ignored: Exception) {
         System.err.println("No git installed on the machine or not on a git repo. UNES will not automate version name and code")
