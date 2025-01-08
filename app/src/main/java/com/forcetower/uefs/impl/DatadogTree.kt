8eef22221ab11144f7a242a754f8752514c56680
João Sena <joaopaulo761@gmail.com>
2025-01-07 21:01:00 -0300

    Dont send debug or lower

--- app/src/main/java/com/forcetower/uefs/impl/DatadogTree.kt
+++ app/src/main/java/com/forcetower/uefs/impl/DatadogTree.kt
@@ -1,5 +1,6 @@
 package com.forcetower.uefs.impl
 
+import android.util.Log
 import com.datadog.android.log.Logger
 import timber.log.Timber
 import javax.inject.Inject
@@ -10,6 +11,8 @@ class DatadogTree @Inject constructor(
     private val logger: Logger
 ) : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        if (priority <= Log.DEBUG) return
+
         val msg = if (tag != null) "$tag: $message" else message
         logger.log(priority, msg, t)
     }
