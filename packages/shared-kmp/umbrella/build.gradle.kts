25e2e5f8a66a5e85da5e7ed66cfefc8bbea8aa2f
João Sena <joaopaulo761@gmail.com>
2026-04-19 16:27:35 -0300

    iOS Swift 6 Strict Concurrency

--- packages/shared-kmp/umbrella/build.gradle.kts
+++ packages/shared-kmp/umbrella/build.gradle.kts
@@ -3,13 +3,33 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 plugins {
     id("melon.kmp-library")
     alias(libs.plugins.metro)
+    alias(libs.plugins.skie)
 }
 
 kotlin {
     val xcf = XCFramework("Umbrella")
-    iosX64 { binaries.framework { baseName = "Umbrella"; xcf.add(this) } }
-    iosArm64 { binaries.framework { baseName = "Umbrella"; xcf.add(this) } }
-    iosSimulatorArm64 { binaries.framework { baseName = "Umbrella"; xcf.add(this) } }
+    val bundleId = "dev.forcetower.melon.umbrella"
+    iosX64 {
+        binaries.framework {
+            baseName = "Umbrella"
+            binaryOption("bundleId", bundleId)
+            xcf.add(this)
+        }
+    }
+    iosArm64 {
+        binaries.framework {
+            baseName = "Umbrella"
+            binaryOption("bundleId", bundleId)
+            xcf.add(this)
+        }
+    }
+    iosSimulatorArm64 {
+        binaries.framework {
+            baseName = "Umbrella"
+            binaryOption("bundleId", bundleId)
+            xcf.add(this)
+        }
+    }
 
     sourceSets {
         commonMain.dependencies {
